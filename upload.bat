ffab5c6a9afa1f9a05e56befd9e7b23cbb99c045
Loop <843581028@qq.com>
2021-07-31 15:46:45 +0800

    upload.bat

--- upload.bat
+++ upload.bat
@@ -4,4 +4,4 @@ set curtime=%date% %time%
 
 set customDomain = "blog.notgame.top"
 
-start cmd /k "pelican content -o output -s publishconf.py && ghp-import -c customDomain -m "Publish site %curtime=%" --no-jekyll -b gh-pages output && git push origin gh-pages"
+start cmd /k "pelican content -o output -s publishconf.py && ghp-import -c %customDomaim% -m "Publish site %curtime=%" --no-jekyll -b gh-pages output && git push origin gh-pages"

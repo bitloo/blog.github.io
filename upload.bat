72c59bf464fecc9a11cca54d1f9e739ea1555813
Loop <843581028@qq.com>
2021-07-31 15:35:43 +0800

    upload.bat

--- upload.bat
+++ upload.bat
@@ -1,2 +1,5 @@
 @echo off
-start cmd /k "inv gh-pages"
+
+set curtime=%date% %time%
+
+start cmd /k "pelican content -o output -s publishconf.py && ghp-import -m "Publish site %curtime=%" --no-jekyll -b gh-pages output && git push origin gh-pages"
